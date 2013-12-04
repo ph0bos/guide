1eeecc0bd87d43c123310185f537226d85a54d1a
Steven Robinson <sm0k3yuk@gmail.com>
2013-12-04 12:26:57 +0000

    Initial check in of integration test

---
+++ src/test/scala/com/pressassociation/guide/resource/CategoryResourceIT.scala
@@ -0,0 +1,19 @@
+package com.pressassociation.guide.resource
+
+import akka.actor.ActorSystem
+import org.scalatra.test.scalatest.{ScalatraSuite}
+import org.scalatest.FunSuite
+
+/**
+ * Created by stevenr on 03/12/2013.
+ */
+class CategoryResourceIT extends ScalatraSuite with FunSuite {
+
+  addServlet(new CategoryResource(ActorSystem()), "/*")
+
+  test("get category list") {
+    get("/") {
+      status should equal (200)
+    }
+  }
+}
